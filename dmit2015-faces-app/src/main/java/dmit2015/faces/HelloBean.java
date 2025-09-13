5c6046a22aa11da351138e4fc0f74e569bee23e0
Sam Wu <swu@nait.ca>
2025-09-13 10:41:25 -0600

    Added missing beans.xml to trigger CDI activation with Apache Tomcat.

--- dmit2015-faces-app/src/main/java/dmit2015/faces/HelloBean.java
+++ dmit2015-faces-app/src/main/java/dmit2015/faces/HelloBean.java
@@ -1,6 +1,8 @@
 package dmit2015.faces;
 
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
 
 @Named("iphone17")
@@ -9,20 +11,22 @@ public class HelloBean {
 
     private String userInput;
 
+    public String getUserInput() {
+        return userInput;
+    }
+
+    public void setUserInput(String userInput) {
+        this.userInput = userInput;
+    }
+
     public String getMessage() {
         return "Hello, " + userInput;
     }
 
     public void submit() {
-        userInput = "out of stock";
+        var message = String.format("Hello %s and welcome to Jakarta Faces world!", userInput);
+        var facesMessage = new FacesMessage(message);
+        FacesContext.getCurrentInstance().addMessage(null, facesMessage);
     }
 
-    public void setMessage(String userInput) {
-        this.userInput = userInput;
-    }
-
-    public String getUserInput() {
-        return userInput;
-    }
-    public void setUserInput(String userInput) {}
 }
